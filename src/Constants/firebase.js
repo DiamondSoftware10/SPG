ee2c6a1090f7ffe154c590ecdf1740d4f023510c
yoshuadupin <djosue@united.edu>
2018-11-16 10:22:32 -0600

    CRUD USER

--- src/Constants/firebase.js
+++ src/Constants/firebase.js
@@ -1,5 +1,5 @@
-import { database } from './firebase';
-import * as firebase from 'firebase'
+import { database } from "./firebase";
+import * as firebase from "firebase";
 
 var config = {
   apiKey: "AIzaSyAlyrXClxsPQr7xsi4g7YYReoN5gfiRDqk",
@@ -13,57 +13,128 @@ firebase.initializeApp(config);
 
 //const firebase = require("firebase");
 const db = firebase.firestore();
-const usersRef = db.collection("users");
+const usersRef = db.collection("spg-project-1").doc("users");
 //CRUD USERS
-export const createUser = (name, last, phone, email, nationality, birth
-  , picture, inversion, creationDate, notification, investedMoney, earnedMoney) => {
+export const createUser = (
+  name,
+  last,
+  phone,
+  email,
+  nationality,
+  birth,
+  picture,
+  inversion,
+  creationDate,
+  notification,
+  investedMoney,
+  earnedMoney
+) => {
   return new Promise((resolve, reject) => {
-    db.collection("users").add({
-      name: name,
-      last: last,
-      phone: phone,
-      email: email,
-      nationality: nationality,
-      //picture
-      //inversion
-      birth: birth,
-      creationDate: creationDate,
-      //notification:notification
-      //cartera de inversion
-      //shopping:
-      investedMoney: investedMoney,
-      earnedMoney: earnedMoney
-    }).then(result => {
-      resolve(true);
-    }).catch(err => {
-      reject(false);
-    });
+    db.collection("users")
+      .add({
+        name: name,
+        last: last,
+        phone: phone,
+        email: email,
+        nationality: nationality,
+        picture: "",
+        inversion: [],
+        birth: birth,
+        creationDate: creationDate,
+        notification: [],
+        wallet: [],
+        shopping: [],
+        investedMoney: investedMoney,
+        earnedMoney: earnedMoney
+      })
+      .then(result => {
+        console.log("Agrego a la base de datos")
+        resolve(true);
+      })
+      .catch(err => {
+        reject(false);
+      });
   });
-}
+};
 
 export const listUsers = () => {
   return new Promise((resolve, reject) => {
     var listUsers = [];
-    
 
-    db.collection("users").get().then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        listUsers.push(doc.data());
+    db.collection("users")
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+          listUsers.push(doc.data());
+          console.log("size:" + listUsers.length);
+        });
+        resolve(listUsers);
       });
-    });
-  })
-}
-
-export const deleteUsers = (id) => {
+  });
+};
+export const updateUser = (
+  id,
+  name,
+  last,
+  phone,
+  email,
+  nationality,
+  birth,
+  picture,
+  inversion,
+  creationDate,
+  notification,
+  investedMoney,
+  earnedMoney
+) => {
   return new Promise((resolve, reject) => {
-    usersRef.child(id).remove().then(result => {
-      resolve(true);
-    }).catch(err => {
-      reject(false);
-    });
-  })
-}
+    db.collection("users")
+      .doc(id)
+      .update({
+        name: name,
+        last: last,
+        phone: phone,
+        email: email,
+        nationality: nationality,
+        picture: "",
+        inversion: [],
+        birth: birth,
+        creationDate: creationDate,
+        notification: [],
+        wallet: [],
+        shopping: [],
+        investedMoney: investedMoney,
+        earnedMoney: earnedMoney
+      })
+      .then(result => {
+        console.log("Borrado con exito");
+        resolve(true);
+      })
+      .catch(err => {
+        console.log("No se pudo borrar");
+
+        reject(false);
+      });
+  });
+};
+
+export const deleteUser = id => {
+  return new Promise((resolve, reject) => {
+    db.collection("users")
+      .doc(id)
+      .delete()
+      .then(result => {
+        console.log("Borrado con exito");
+        resolve(true);
+      })
+      .catch(err => {
+        console.log("No se pudo borrar");
+
+        reject(false);
+      });
+  });
+};
 // Required for side-effects
-require("firebase/firestore")
+require("firebase/firestore");
