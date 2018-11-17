242fcad01ae8d3bd432eda2bde99fe0f19b27434
Memo <guillelopez_13@hotmail.com>
2018-11-16 18:41:37 -0600

    landing page ready for everything

---
+++ src/components/Proyectos.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Proyectos extends Component {
+  render() {
+    return (
+      <div>
+        PROYECTOS
+      </div>
+    )
+  }
+}
