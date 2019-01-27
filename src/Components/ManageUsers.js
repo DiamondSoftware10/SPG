3ed38eb55d62f0196f932c67d5ad08856662df8f
CascoEresTu <k_k_ryuten@live.com>
2019-01-26 18:55:01 -0600

    Revert "metodo de delete"
    This reverts commit 733d0e05c0fdc8508ac31d1cd3b7595284204159.

--- src/Components/ManageUsers.js
+++ src/Components/ManageUsers.js
@@ -14,56 +14,59 @@ class ManageUsers extends Component {
 
         this.props = props;
         this.controller = new AbortController();
-
-        this.state = {
-            data: [],
+        
+        this.state ={
+            data : [],
         }
 
-        this.columns = [{
+        this.columns = [
+            {
                 Header: "ID",
                 accessor: "id",
                 width: 70,
-            },
-            {
+              },
+              {
                 Header: "First Name",
                 accessor: "nombre",
                 width: 70,
-            },
-            {
+              },
+              {
                 Header: "Last Name",
                 accessor: "apellido",
                 width: 70,
-            },
-            {
+              },
+              {
                 Header: "Type",
                 accessor: "accType",
                 width: 70,
-            }
+              }
         ];
         //this.error = this.error.bind(this);
     }
 
     componentDidMount() {
         const database = fire.firestore();
-        // database.settings({timestampsInSnapshots: true});
+       // database.settings({timestampsInSnapshots: true});
         const collection = database.collection('users');
 
         collection.get().then(snapshot => {
-            const data = [];
+            const data =[];
 
-            snapshot.forEach(doc => {
+              snapshot.forEach(doc => {
                 var type = 'User';
-                if (doc.data().accType == 0) {
+                if (doc.data().accType == 0 ){
                     type = 'Admin'
                 }
 
-                const admin = {
-                    id: doc.id,
-                    nombre: doc.data().nombre,
-                    apellido: doc.data().apellido,
-                    accType: type,
-                }
-                data.push(admin);
+
+              const admin ={
+                  id : doc.data().key,
+                  nombre: doc.data().nombre,
+                  apellido: doc.data().apellido,
+                  accType: type,
+
+              }
+              data.push(admin);
             });
 
             this.setState(prevState => {
@@ -71,26 +74,22 @@ class ManageUsers extends Component {
                     data: [...prevState.data, ...data]
                 };
             });
+        
         });
     }
 
-    DeleteUser(id) {
-        const db = fire.firestore();
-        db.collection("users").doc(id.toString()).delete().then(function () {
-            console.log("Document successfully deleted!");
-        }).catch(function (error) {
-            console.error("Error removing document: ", error);
-        });
-    }
 
-    render() {
-        return (
-            <div>
-                <ReactTable data={this.state.data} columns={this.columns} filterable />
-            </div>
-        );
 
-    }
+render() {
+    
+
+    return (
+        <div>
+            <ReactTable data={this.state.data} columns={this.columns} filterable />
+        </div>
+      );
+
+}
 }
 
 
