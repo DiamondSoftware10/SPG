04ebd30bfc25a22456f65a01707c81a33dbc080e
David Leonel Nuñez Elvir <davidelvir2007@hotmail.com>
2019-02-14 21:27:10 -0600

    manage users

--- src/Components/ManageUsers.js
+++ src/Components/ManageUsers.js
@@ -3,17 +3,15 @@ import React, {
 } from 'react';
 import fire from "../Firebase/Fire";
 import 'firebase/database';
-
+import './Manage.css'
 import ReactTable from "react-table";
 import "react-table/react-table.css";
-//que pedos 
 
 class ManageUsers extends Component {
     constructor(props) {
         super(props);
 
         this.props = props;
-        //this.controller = new AbortController();
 
         this.state = {
             data: [],
@@ -26,6 +24,11 @@ class ManageUsers extends Component {
                 Header: "ID",
                 accessor: "id",
                 width: 300,
+            },
+            {
+                Header: "Estado de cuenta",
+                accessor: "active",
+                width: 100,
             },
             {
                 Header: "First Name",
@@ -43,19 +46,8 @@ class ManageUsers extends Component {
                 width: 100,
             }
             ,
-            {
-                Header: "Delete",
-                id: 'deleteUserButton',
-                accessor: row => (
-                    
-                        <button onClick={() => this.DeleteUser(this.state.selectedID)}>Eliminar</button>
-                    
-                ),
-                // width: 100,
-                filterable: false,
-            },
+
         ];
-        //this.error = this.error.bind(this);
     }
 
 
@@ -64,25 +56,24 @@ class ManageUsers extends Component {
 
     componentDidMount() {
         const database = fire.firestore();
-        // database.settings({timestampsInSnapshots: true});
         const collection = database.collection('users');
 
         collection.get().then(snapshot => {
             const data = [];
             snapshot.docs.forEach(doc => {
-                if (doc.data().active) {
-                    var type = 'User';
-                    if (doc.data().accType == 0) {
-                        type = 'Admin'
-                    }
-                    const admin = {
-                        id: doc.id,
-                        nombre: doc.data().nombre,
-                        apellido: doc.data().apellido,
-                        accType: type,
-                    }
-                    data.push(admin);
+                var type = 'User';
+                if (doc.data().accType == 0) {
+                    type = 'Admin'
                 }
+                const admin = {
+                    id: doc.id,
+                    nombre: doc.data().nombre,
+                    apellido: doc.data().apellido,
+                    accType: type,
+                    active: doc.data().active ? "activo" : "inactivo"
+                }
+                data.push(admin);
+
 
             });
 
@@ -94,36 +85,6 @@ class ManageUsers extends Component {
         });
     }
 
-
-    /*async RefreshData() {
-         this.setState=({data : []})
-         
-         
-          const db = fire.firestore();
-     ///db.settings({ timestampsInSnapshots: true});
-         db.collection('users').get().then((snapshot) => {
-             const datos =[];
-         snapshot.docs.forEach(doc => {
-             var type = 'User';
-             if (doc.data().accType == 0 ){
-                 type = 'Admin'
-             }
-             const admin ={
-                 id : doc.id,
-                 nombre: doc.data().nombre,    
-                 apellido: doc.data().apellido,
-                 accType: type,
-             }
-             datos.push(admin);
-             this.setState({ data : datos }) 
-       });
-     });
-         
-    } */
-
-
-
-
     deleteRow(index) {
         var data = [...this.state.data];
         data.splice(index, 1);
@@ -131,32 +92,20 @@ class ManageUsers extends Component {
     }
 
 
+    UndoDelete() {
 
-    DeleteUser() {
-        if (window.confirm("¿ Está seguro que desea elminar el usuario ?")) {
-            console.log('que pedos')
-            console.log(this.state.selected);
-            console.log(this.state.selectedID);
-
+        if (window.confirm("¿ Está seguro que desea reactivar el usuario ?")) {
             const db = fire.firestore();
-            db.collection("users").doc(this.state.selectedID).update({ active: false });
-            /*db.collection("users").doc(this.state.selectedID).delete().then(function () {
-                console.log("Document successfully deleted!");
-            }).catch(function (error) {
-                console.error("Error removing document: ", error);
-            });*/
-
+            db.collection("users").doc(this.state.selectedID).update({ active: true });
             this.setState({ data: [] })
 
             const database = fire.firestore();
-            // database.settings({timestampsInSnapshots: true});
             const collection = database.collection('users');
-
-
             collection.get().then(snapshot => {
                 const data = [];
                 snapshot.docs.forEach(doc => {
-                    if (doc.data().active) {
+
+                    if (doc.data().accType < 2) {
                         var type = 'User';
                         if (doc.data().accType == 0) {
                             type = 'Admin'
@@ -166,9 +115,46 @@ class ManageUsers extends Component {
                             nombre: doc.data().nombre,
                             apellido: doc.data().apellido,
                             accType: type,
+                            active: doc.data().active ? "activo" : "inactivo"
                         }
                         data.push(admin);
                     }
+
+                });
+
+                this.setState(prevState => {
+                    return {
+                        data: [...prevState.data, ...data]
+                    };
+                });
+            });
+        }
+    }
+    DeleteUser() {
+        if (window.confirm("¿ Está seguro que desea elminar el usuario ?")) {
+            const db = fire.firestore();
+            db.collection("users").doc(this.state.selectedID).update({ active: false });
+            this.setState({ data: [] })
+
+            const database = fire.firestore();
+            const collection = database.collection('users');
+            collection.get().then(snapshot => {
+                const data = [];
+                snapshot.docs.forEach(doc => {
+
+                    var type = 'User';
+                    if (doc.data().accType == 0) {
+                        type = 'Admin'
+                    }
+                    const admin = {
+                        id: doc.id,
+                        nombre: doc.data().nombre,
+                        apellido: doc.data().apellido,
+                        accType: type,
+                        active: doc.data().active ? "activo" : "inactivo"
+                    }
+                    data.push(admin);
+
                 });
 
                 this.setState(prevState => {
@@ -183,34 +169,34 @@ class ManageUsers extends Component {
     }
 
     render() {
-        // this.RefreshData();
-
-
         return (
+            <div >
+                <button onClick={() => this.DeleteUser(this.state.selectedID)}>Eliminar</button>
+                <button onClick={() => this.UndoDelete(this.state.selectedID)}>Reactivar Cuenta</button>
+                <div id="div-table">
 
-            <div>
-                
-                <ReactTable pageSizeOptions={[5,5,5,5,5,5]} data={this.state.data} columns={this.columns} filterable
-                    getTrProps={(state, rowInfo) => {
-                        if (rowInfo && rowInfo.row) {
-                            return {
-                                onClick: (e) => {
-                                    this.setState({
-                                        selectedID: rowInfo.original.id,
-                                        selected: rowInfo.index
-                                    })
-                                },
-                                style: {
-                                    background: rowInfo.index === this.state.selected ? 'green' : 'white',
-                                    color: rowInfo.index === this.state.selected ? 'white' : 'black'
+                    <ReactTable data={this.state.data} columns={this.columns} filterable
+                        getTrProps={(state, rowInfo) => {
+                            if (rowInfo && rowInfo.row) {
+                                return {
+                                    onClick: (e) => {
+                                        this.setState({
+                                            selectedID: rowInfo.original.id,
+                                            selected: rowInfo.index
+                                        })
+                                    },
+                                    style: {
+                                        background: rowInfo.index === this.state.selected ? 'green' : 'white',
+                                        color: rowInfo.index === this.state.selected ? 'white' : 'black'
+                                    }
                                 }
+                            } else {
+                                return {}
                             }
-                        } else {
-                            return {}
                         }
-                    }
-                    }
-                />
+                        }
+                    />
+                </div>
             </div>
         );
 
