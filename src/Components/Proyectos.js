9b0dac3d663dab7c629f9e4879b7f40e7944f69e
Mario Sánchez <Maslz980@outlook.com>
2019-03-06 22:07:06 -0600

    Cargar algunos proyectos

--- src/Components/Proyectos.js
+++ src/Components/Proyectos.js
@@ -4,6 +4,7 @@ import * as routes from '../Constants/Routes';
 import MapContainer from "../Components/GoogleMapsContainer"
 import InfoCard from "./Infocards";
 import ReactDOM from 'react-dom';
+import { Pagination } from 'antd';
 
 import './Proyectos.css'
 
@@ -22,8 +23,8 @@ export default class Proyectos extends Component {
 
     this.state = {
       projects: [],
-      center: {lat: 0, lng:0},
-      user : null
+      center: { lat: 0, lng: 0 },
+      user: null
     }
 
 
@@ -38,15 +39,15 @@ export default class Proyectos extends Component {
     console.log(event.target.value);
   };
 
-  changeLocation(arg){
+  changeLocation(arg) {
     console.log(arg)
     this.setState({
       center: arg
     })
-    
+
   }
 
-  async getProyectos() {
+  /*async getProyectos() {
     await fire.auth().onAuthStateChanged(user => {
 
         projectRef.get().then((querySnapshot) => {
@@ -75,24 +76,60 @@ export default class Proyectos extends Component {
       
     })
 
+  }*/
+
+
+  async getProyectos() {
+
+
+    await fire.auth().onAuthStateChanged(user => {
+
+      var first = db.collection("projects").orderBy("raisedMoney").limit(3);
+
+      first.get().then((querySnapshot) => {
+        let data = [];
+
+        querySnapshot.forEach((doc) => {
+
+          console.log(doc.data().title);
+          if (doc.data().available !== false) {
+            data.push(doc.data());
+          }
+
+          var lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
+
+        });
+        // var exp = document.getElementById("cards-div");
+
+        //console.log(proyectos.length);
+        for (var i = 0; i < data.length / 2; i++) {
+          console.log(data[i]);
+
+        }
+        //guardando los proyectos
+        this.setState({ projects: data })
+      });
+
+    })
+
   }
 
   componentDidMount() {
     this.getProyectos();
 
-    
+
   }
 
   componentWillUnmount() {
-    
-  }
-
-  authorizedToOpen(){
 
   }
-  
 
-   render() {
+  authorizedToOpen() {
+
+  }
+
+
+  render() {
     const style = {
       /*width: '50vw',
       height: '75vh',*/
@@ -107,14 +144,14 @@ export default class Proyectos extends Component {
     let cards = this.state.projects.map((doc, i) => {
       console.log("card " + i);
       console.log(doc.picProject)
-   
-      
+
+
       return (
         <InfoCard
-          changeLocation = {this.changeLocation}
+          changeLocation={this.changeLocation}
           key={i}
           id={doc.id}
-          pic = {doc.picProject}
+          pic={doc.picProject}
           title={doc.title}
           location={doc.locate}
           lat={doc.coordinates._lat}
@@ -131,67 +168,68 @@ export default class Proyectos extends Component {
     return (
 
       <div className="info-cont">
-      <h1 className="main-title">Proyectos</h1>
-      
-      <div id="cards-div">
-        {cards}
-      </div>
-      
+        <h1 className="main-title">Proyectos</h1>
 
-      <div className="modal" id="mapModal">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h4 className="modal-title">Ubicación del Proyecto</h4>
-              <button type="button" className="close" data-dismiss="modal">&times;</button>
-            </div>
+        <div id="cards-div">
+          {cards}
+        </div>
 
-            <div className="modal-body">
 
-              <div className="container">
+        <div className="modal" id="mapModal">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title">Ubicación del Proyecto</h4>
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+              </div>
 
-                <div className="card" style={style}>
-                  <MapContainer center={{
-                    lat: this.state.center.lat,
-                    lng: this.state.center.lng
-                  }} />
-                </div>
+              <div className="modal-body">
 
-                <div className="card" style={style}>
-                  <div className="form-group col-sm">
-                    <label htmlFor="usr">Nombre del lugar:</label>
-                    <input
-                      onChange={this.handleChange('lugar')}
-                      type="text" className="form-control" id="lugar" />
+                <div className="container">
+
+                  <div className="card" style={style}>
+                    <MapContainer center={{
+                      lat: this.state.center.lat,
+                      lng: this.state.center.lng
+                    }} />
                   </div>
-                  <div className="form-group col-sm">
-                    <label htmlFor="usr">Dirección:</label>
-                    <input
-                      onChange={this.handleChange('direccion')}
-                      type="text" className="form-control" id="direccion" />
+
+                  <div className="card" style={style}>
+                    <div className="form-group col-sm">
+                      <label htmlFor="usr">Nombre del lugar:</label>
+                      <input
+                        onChange={this.handleChange('lugar')}
+                        type="text" className="form-control" id="lugar" />
+                    </div>
+                    <div className="form-group col-sm">
+                      <label htmlFor="usr">Dirección:</label>
+                      <input
+                        onChange={this.handleChange('direccion')}
+                        type="text" className="form-control" id="direccion" />
+                    </div>
                   </div>
+
                 </div>
 
               </div>
-
-            </div>
-            <div className="modal-footer">
-              {/*<button onClick={() => this.recenter} type="button" className="btn btn-secondary">
+              <div className="modal-footer">
+                {/*<button onClick={() => this.recenter} type="button" className="btn btn-secondary">
                                   RECENTER
                               </button>*/}
 
-              <button onClick={this.saveLocation} type="button" className="btn btn-secondary" data-dismiss="modal">
-                OK
+                <button onClick={this.saveLocation} type="button" className="btn btn-secondary" data-dismiss="modal">
+                  OK
                               </button>
-              <button type="button" className="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                <button type="button" className="btn btn-danger" data-dismiss="modal">Cancelar</button>
+              </div>
             </div>
+
           </div>
 
         </div>
-
+        <Pagination size="small" total={50} />
       </div>
-    </div>
-      
+
     )
   }
 }
